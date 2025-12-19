8b82135552aad2d56e0498819c3a827a329a2cfb
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-12-19 16:42:54 +0400

    Initialize sample http server with chi mux

---
+++ cmd/api/api.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type config struct {
+	addr string
+}
+
+type application struct {
+	config config
+}
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RealIP)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("v1 index route"))
+		})
+	})
+
+	return r
+}
+
+func (app *application) run(mux http.Handler) error {
+	srv := http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	fmt.Printf("App started at port %s\n", app.config.addr)
+	return srv.ListenAndServe()
+}
